fc26a489f2bfac41e0be387115128d2f671fb7ad
Harold Beckerman <halbeckerman@yahoo.com>
2019-01-16 22:01:11 -0600

    fully removed submodule, added hamburger, need to adjust placement of title so it doesnt move on text insertion

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const textToGrow = document.getElementById("text-to-grow");
 document.getElementById("fade-in-text").classList.add("fade-in");
-let textToAdd = "...or maybe just really want.";
+let textToAdd = "...or maybe just really, REALLY want.";
 let textToAddArray = textToAdd.split(" ");
 addToText = () => {
   for(var i=0; i<textToAddArray.length; i++) {
@@ -12,8 +12,30 @@ addToText = () => {
     }, 700+i*700);
   }
 }
-
-// const addToFadeIn =
-
-// const addWord(word)
 addToText()
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById('target');
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});

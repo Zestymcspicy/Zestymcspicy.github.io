3742b5e2031794264c47889d0baccf455c962d1c
Zestymcspicy <halbeckerman@yahoo.com>
2020-03-06 23:51:30 -0600

    small punctuation fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,42 @@
 'use-strict'
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var server = require('browser-sync').create();
 
-sass.compiler = require('node-sass');
+// sass.compiler = require('node-sass');
+function serve() {
+  server.init({
+    server: {
+      baseDir: './',
+    }
+  })
 
-gulp.task('default', ['browser-sync'], function() {
-  gulp.watch("./sass/*.scss", ['sass']);
-  gulp.watch("css/*.css", browserSync.reload);
-  gulp.watch("index.html", browserSync.reload);
-  gulp.watch("js/*.js", browserSync.reload);
-})
+  gulp.watch([
+    '*.html',
+    '*.js',
+    '*.css'
+  ]).on('change', server.reload);
+}
+// gulp.task('default', ['browser-sync'], function() {
+//   gulp.watch("./sass/*.scss", ['sass']);
+//   gulp.watch("css/*.css", browserSync.reload);
+//   gulp.watch("index.html", browserSync.reload);
+//   gulp.watch("js/*.js", browserSync.reload);
+// })
+//
+// gulp.task('browser-sync', function() {
+//   browserSync.init({
+//     server: "./"
+//   });
+// })
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: "./"
-  });
-})
-
-gulp.task('sass', function () {
+function sass() {
+  gulp.watch(['./sass/*.scss'])
   return gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'))
-})
+}
 
-
+exports.serve = serve;
 
 // gulp.task('default', gulp.series('browser-sync', 'watch-reload'))
